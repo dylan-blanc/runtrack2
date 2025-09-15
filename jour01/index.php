86934d97fcd4bc7e4ccc4664ca65e1063a635691
Dylan <dylan.blanc@laplateforme.io>
2025-09-15 13:27:34 +0000

    sdf

---
+++ jour01/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+
+    <meta charset="utf-8">
+    <title>Blabla</title>
+    <link href="Atemplate.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <header>
+
+    </header>
+
+
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
