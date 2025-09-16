50671ddab79adf5b0411d4f66d93333963706802
Dylan <dylan.blanc@laplateforme.io>
2025-09-16 09:44:43 +0200

    jour01 fini

---
+++ jour01/job02/index.php
@@ -0,0 +1,8 @@
+<?php
+$hello = "Hello  ";
+$laplat = "LaPlateforme ";
+echo $hello . $laplat . " <br> "; // ceci est un commentaire //
+echo "$hello $laplat <br> "; /* ceci est un commentaire
+sur plusieurs lignes */
+
+?>
---
+++ jour01/job03/index.php
@@ -0,0 +1,63 @@
+<?php
+$monBool = true;
+$monEntier = 42;
+$maChaine = "Bonjour bonjour";
+$monFloat = 2.012;
+
+$tbl = "<table border='1'>
+        <tr>
+            <th>Type</th>
+            <th>Nom</th>
+            <th>Valeur</th>
+        </tr>
+        <tr>
+            <td>Boolean</td>
+            <td>monBool</td>
+            <td>$monBool</td>
+        </tr>
+        <tr>
+            <td>Entier</td>
+            <td>monEntier</td>
+            <td>$monEntier</td>
+        </tr>
+        <tr>
+            <td>Chaine</td>
+            <td>maChaine</td>
+            <td>$maChaine</td>
+        </tr>
+        <tr>
+            <td>Float</td>
+            <td>monFloat</td>
+            <td>$monFloat</td>
+        </tr>
+    </table>";
+
+echo $tbl;
+?>
+
+
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+
+	<meta charset="utf-8">
+	<title>Blabla</title>
+	<link href="Atemplate.css" rel="stylesheet">
+
+</head>
+
+<body>
+	<header>
+		<h1>TEST</h1>
+	</header>
+
+
+
+	<footer>
+
+	</footer>
+</body>
+
+</html>
