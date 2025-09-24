0e50cd99cc9ba44d4a722c6d7bd3b6f7466705cb
Dylan <dylan.blanc@laplateforme.io>
2025-09-24 10:32:31 +0200

    fin exercice 07 - job 07

--- jour07/job07/index.php
+++ jour07/job07/index.php
@@ -1,15 +1,66 @@
 <?php
 
+$decalage = 2;
 
+$str = "La Plateforme test de phrase Inutile A b c d E F G H I J K L M N O P Q R S T U V W X Y Z";
 
+function selection($str, $decalage)
+{
+    if (isset($_GET["fonction"])) {
+        switch ($_GET["fonction"]) {
+            case "gras":
+                return gras($str);
+            case "cesar":
+                return cesar($str, $decalage);
+            case "plateforme":
+                return plateforme($str);
+            default:
+                return $str;
+        }
+    }
+    return $str;
+}
 
+function gras($str)
+{
+    $phrase = explode(' ', $str); // $phrase sépare la phrase en lettre individuel "explode" dont la source est : $str
+    foreach ($phrase as &$lettre) {  // parcourt chaque mot de la phrase, le mot actuel devenant $lettre
+        if (isset($lettre[0]) && ctype_upper($lettre[0])) {  //verifie si la lettre est une majuscule
+            $lettre = '<b>' . $lettre[0] . '</b>' . substr($lettre, 1); // met en gras la $lettre si elle est une majuscule
+        }
+    }
+    // On recompose la phrase depuis $phrase et donc $str
+    return implode(' ', $phrase);
+}
 
+function cesar($str, $decalage)
+{
+    $result = "";
+    for ($i = 0; $i < strlen($str); $i++) {
+        $char = $str[$i];
+        // Décale seulement les lettres
+        if (ctype_alpha($char)) {
+            $ascii = ord($char);
+            $base = ctype_upper($char) ? ord('A') : ord('a');
+            $result .= chr(($ascii - $base + $decalage) % 26 + $base);
+        } else {
+            $result .= $char;
+        }
+    }
+    return $result;
+}
 
+function plateforme ($str)
+{
+    return str_replace("me", "me_", $str);  // "str_replace" remplace le premier contenu des guillemets par le deuxieme contenu des guillemets dont la source est $str exemple:  str_replace("Test", "test_", $str);
+}
 
-
-
+echo selection($str, $decalage);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
 
 <head>
     <meta charset="utf-8" />
@@ -22,8 +73,8 @@
 <body>
     <header>
         <h1>o/</h1>
-        <p> BLABLABLA, TEST, BLABLABLA
-            <br> Test changement de police d'ecriture
+        <p> 
+            <br> 
         </p>
     </header>
 
@@ -41,10 +92,10 @@
 
                 <li>
                     <label for="style">Style</label>
-                    <select class="dropdown" id="style" name="style" size="<?= (isset($_GET['style']) && $_GET['style'] === 'style3') ? 4 : null; ?>"> <!-- si style3 est sélectionné, afficher 4 options sinon l'affichage de base -->
-                        <option class="option0" value="fonction1">Gras</option>
-                        <option class="option1" value="fonction2">Cesar</option>
-                        <option class="option2" value="fonction3">Plateforme</option>
+                    <select class="dropdown" id="fonction" name="fonction" size="<?= (isset($_GET['style']) && $_GET['style'] === 'style3') ? 4 : null; ?>"> <!-- si style3 est sélectionné, afficher 4 options sinon l'affichage de base -->
+                        <option class="option0" value="gras">Gras</option>
+                        <option class="option1" value="cesar">Cesar</option>
+                        <option class="option2" value="plateforme">Plateforme</option>
                     </select>
                 </li>
                 <!-- <li>
